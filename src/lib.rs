1ebbea212fa7929a6aa56b9e9b7f65bce488dee3
jmartin <andriantsiferanamartin.ext@orange.com>
2022-10-10 17:00:30 +0300

    Code optimisation string

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,13 @@ mod booklib {
     use std::{collections::HashMap, ops::Deref};
 
     #[derive(Debug, PartialEq)]
-    pub struct Book<'a> {
-        pub title: &'a str,
+    pub struct Book {
+        pub title: String,
         pub price: f32,
     }
 
-    impl<'a> Book<'a> {
-        pub fn new(title: &'a str) -> Self {
+    impl Book {
+        pub fn new(title: String) -> Self {
             Book {
                 title: title,
                 price: 8.0,
@@ -18,31 +18,31 @@ mod booklib {
     }
 
     #[derive(Debug, PartialEq)]
-    pub struct BookSet<'a> {
-        pub book: Book<'a>,
+    pub struct BookSet {
+        pub book: Book,
         pub count: usize,
     }
 
     #[derive(Debug, PartialEq)]
-    pub struct BookCart<'a> {
-        pub bookset: Vec<BookSet<'a>>,
+    pub struct BookCart {
+        pub bookset: Vec<BookSet>,
     }
 
-    impl<'a> Deref for BookCart<'a> {
-        type Target = Vec<BookSet<'a>>;
+    impl Deref for BookCart {
+        type Target = Vec<BookSet>;
         fn deref(&self) -> &Self::Target {
             &self.bookset
         }
     }
 
-    impl<'a> BookCart<'a> {
+    impl BookCart {
         pub fn new() -> Self {
             BookCart {
                 bookset: Vec::new(),
             }
         }
 
-        pub fn add(&mut self, book: Book<'a>) {
+        pub fn add(&mut self, book: Book) {
             let it = self.bookset.iter_mut();
             let mut exist = false;
             for item in it {
@@ -61,8 +61,7 @@ mod booklib {
         }
     }
 
-    pub fn calculate_price<'a>(bookcart: &BookCart<'a>) -> f32 {
-        let bookset = &*bookcart;
+    fn book_nb_copy(bookset: &BookCart) -> HashMap<u8, usize> {
         //get number of book'type in cart
         let count_book_type = bookset.len();
         //hold count of book peer type
@@ -70,8 +69,13 @@ mod booklib {
         for (k, v) in bookset.iter().enumerate() {
             bookset_counter.insert(k as u8, v.count);
         }
+        return bookset_counter;
+    }
 
-        let total_price = internal_calculate_price(&mut bookset_counter);
+    pub fn calculate_price<'a>(bookcart: &BookCart) -> f32 {
+        let bookset = &*bookcart;
+        let mut bookset_counter = book_nb_copy(bookset);
+        let total_price = calculate_price_with_discount(&mut bookset_counter);
         return total_price;
     }
 
@@ -84,7 +88,7 @@ mod booklib {
         counter.retain(|_, v| *v > 0);
     }
 
-    fn internal_calculate_price(bookset_counter: &mut HashMap<u8, usize>) -> f32 {
+    fn calculate_price_with_discount(bookset_counter: &mut HashMap<u8, usize>) -> f32 {
         let size = bookset_counter.len();
 
         match size {
@@ -98,7 +102,7 @@ mod booklib {
                 };
                 let price_of_couple: f32 = 8.0 * size as f32 * discount;
                 decrease_counter(bookset_counter);
-                return price_of_couple + internal_calculate_price(bookset_counter);
+                return price_of_couple + calculate_price_with_discount(bookset_counter);
             }
             1 => {
                 return 8.0 * *bookset_counter.values().next().unwrap() as f32;
@@ -117,10 +121,10 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let b = Book::new("I");
+        let b = Book::new("I".to_owned());
         assert_eq!(
             Book {
-                title: "I",
+                title: "I".to_owned(),
                 price: 8.0
             },
             b
@@ -130,22 +134,22 @@ mod tests {
     #[test]
     fn test_add_basket_book() {
         let mut bookcart = BookCart::new();
-        bookcart.add(Book::new("I"));
-        bookcart.add(Book::new("II"));
-        bookcart.add(Book::new("I"));
+        bookcart.add(Book::new("I".to_owned()));
+        bookcart.add(Book::new("II".to_owned()));
+        bookcart.add(Book::new("I".to_owned()));
 
         let expected = BookCart {
             bookset: vec![
                 BookSet {
                     book: Book {
-                        title: "I",
+                        title: "I".to_owned(),
                         price: 8.0,
                     },
                     count: 2,
                 },
                 BookSet {
                     book: Book {
-                        title: "II",
+                        title: "II".to_owned(),
                         price: 8.0,
                     },
                     count: 1,
@@ -164,50 +168,50 @@ mod tests {
     #[test]
     fn test_not_empty_cart() {
         let mut bookcart = BookCart::new();
-        bookcart.add(Book::new("I"));
+        bookcart.add(Book::new("I".to_owned()));
         assert_ne!(0.0, calculate_price(&bookcart));
     }
 
     #[test]
     fn test_one_item_in_cart() {
         let mut bookcart = BookCart::new();
-        bookcart.add(Book::new("I"));
+        bookcart.add(Book::new("I".to_owned()));
         assert_eq!(8.0, calculate_price(&bookcart));
     }
 
     #[test]
     fn test_111() {
         let mut bookcart = BookCart::new();
-        bookcart.add(Book::new("I"));
-        bookcart.add(Book::new("I"));
-        bookcart.add(Book::new("I"));
+        bookcart.add(Book::new("I".to_owned()));
+        bookcart.add(Book::new("I".to_owned()));
+        bookcart.add(Book::new("I".to_owned()));
         assert_eq!(24.0, calculate_price(&bookcart));
     }
 
     #[test]
     fn test_1_2() {
         let mut bookcart = BookCart::new();
-        bookcart.add(Book::new("I"));
-        bookcart.add(Book::new("II"));
+        bookcart.add(Book::new("I".to_owned()));
+        bookcart.add(Book::new("II".to_owned()));
         assert_eq!(8.0 * 2.0 * 0.95, calculate_price(&bookcart));
     }
 
     #[test]
     fn test_11_2() {
         let mut bookcart = BookCart::new();
-        bookcart.add(Book::new("I"));
-        bookcart.add(Book::new("I"));
-        bookcart.add(Book::new("II"));
+        bookcart.add(Book::new("I".to_owned()));
+        bookcart.add(Book::new("I".to_owned()));
+        bookcart.add(Book::new("II".to_owned()));
         assert_eq!((8.0 * 2.0 * 0.95) + 8.0, calculate_price(&bookcart));
     }
 
     #[test]
     fn test_111_2() {
         let mut bookcart = BookCart::new();
-        bookcart.add(Book::new("I"));
-        bookcart.add(Book::new("I"));
-        bookcart.add(Book::new("I"));
-        bookcart.add(Book::new("II"));
+        bookcart.add(Book::new("I".to_owned()));
+        bookcart.add(Book::new("I".to_owned()));
+        bookcart.add(Book::new("I".to_owned()));
+        bookcart.add(Book::new("II".to_owned()));
         assert_eq!(
             (8.0 * 2.0 * 0.95) + 8.0 * 2 as f32,
             calculate_price(&bookcart)
@@ -217,77 +221,77 @@ mod tests {
     #[test]
     fn test_111_22() {
         let mut bookcart = BookCart::new();
-        bookcart.add(Book::new("I"));
-        bookcart.add(Book::new("I"));
-        bookcart.add(Book::new("I"));
-        bookcart.add(Book::new("II"));
-        bookcart.add(Book::new("II"));
+        bookcart.add(Book::new("I".to_owned()));
+        bookcart.add(Book::new("I".to_owned()));
+        bookcart.add(Book::new("I".to_owned()));
+        bookcart.add(Book::new("II".to_owned()));
+        bookcart.add(Book::new("II".to_owned()));
         assert_eq!((8.0 * 4.0 * 0.95) + 8.0, calculate_price(&bookcart));
     }
 
     #[test]
     fn test_1_2_3() {
         let mut bookcart = BookCart::new();
-        bookcart.add(Book::new("I"));
-        bookcart.add(Book::new("II"));
-        bookcart.add(Book::new("III"));
+        bookcart.add(Book::new("I".to_owned()));
+        bookcart.add(Book::new("II".to_owned()));
+        bookcart.add(Book::new("III".to_owned()));
         assert_eq!(8.0 * 3.0 * 0.90, calculate_price(&bookcart));
     }
 
     #[test]
     fn test_1_2_33() {
         let mut bookcart = BookCart::new();
-        bookcart.add(Book::new("I"));
-        bookcart.add(Book::new("II"));
-        bookcart.add(Book::new("III"));
-        bookcart.add(Book::new("III"));
-        bookcart.add(Book::new("III"));
+        bookcart.add(Book::new("I".to_owned()));
+        bookcart.add(Book::new("II".to_owned()));
+        bookcart.add(Book::new("III".to_owned()));
+        bookcart.add(Book::new("III".to_owned()));
+        bookcart.add(Book::new("III".to_owned()));
         assert_eq!((8.0 * 3.0 * 0.90) + (8.0 * 2.0), calculate_price(&bookcart));
     }
 
     #[test]
     fn test_1_2_3_4() {
         let mut bookcart = BookCart::new();
-        bookcart.add(Book::new("I"));
-        bookcart.add(Book::new("II"));
-        bookcart.add(Book::new("III"));
-        bookcart.add(Book::new("IV"));
+        bookcart.add(Book::new("I".to_owned()));
+        bookcart.add(Book::new("II".to_owned()));
+        bookcart.add(Book::new("III".to_owned()));
+        bookcart.add(Book::new("IV".to_owned()));
         assert_eq!(8.0 * 4.0 * 0.80, calculate_price(&bookcart));
     }
 
     #[test]
     fn test_1_2_3_4_5() {
         let mut bookcart = BookCart::new();
-        bookcart.add(Book::new("I"));
-        bookcart.add(Book::new("II"));
-        bookcart.add(Book::new("III"));
-        bookcart.add(Book::new("IV"));
-        bookcart.add(Book::new("V"));
+        bookcart.add(Book::new("I".to_owned()));
+        bookcart.add(Book::new("II".to_owned()));
+        bookcart.add(Book::new("III".to_owned()));
+        bookcart.add(Book::new("IV".to_owned()));
+        bookcart.add(Book::new("V".to_owned()));
         assert_eq!(8.0 * 5.0 * 0.75, calculate_price(&bookcart));
     }
 
     #[test]
     fn test_1x6_2_3x3_4_5x3() {
         let mut bookcart = BookCart::new();
-        bookcart.add(Book::new("I"));
-        bookcart.add(Book::new("I"));
-        bookcart.add(Book::new("I"));
-        bookcart.add(Book::new("I"));
-        bookcart.add(Book::new("I"));
-        bookcart.add(Book::new("I"));
-        bookcart.add(Book::new("I"));
+        bookcart.add(Book::new("I".to_owned()));
+        bookcart.add(Book::new("I".to_owned()));
+        bookcart.add(Book::new("I".to_owned()));
+        bookcart.add(Book::new("I".to_owned()));
+        bookcart.add(Book::new("I".to_owned()));
+        bookcart.add(Book::new("I".to_owned()));
+        bookcart.add(Book::new("I".to_owned()));
 
-        bookcart.add(Book::new("II"));
+        bookcart.add(Book::new("II".to_owned()));
 
-        bookcart.add(Book::new("III"));
-        bookcart.add(Book::new("III"));
-        bookcart.add(Book::new("III"));
+        bookcart.add(Book::new("III".to_owned()));
+        bookcart.add(Book::new("III".to_owned()));
+        bookcart.add(Book::new("III".to_owned()));
 
-        bookcart.add(Book::new("IV"));
+        bookcart.add(Book::new("IV".to_owned()));
 
-        bookcart.add(Book::new("V"));
-        bookcart.add(Book::new("V"));
-        bookcart.add(Book::new("V"));
+        bookcart.add(Book::new("V".to_owned()));
+        bookcart.add(Book::new("V".to_owned()));
+        bookcart.add(Book::new("V".to_owned()));
         assert_eq!(
             (8.0 * 5.0 * 0.75) + (8.0 * 3.0 * 0.90) * 2.0 + (8.0 * 4.0),
             calculate_price(&bookcart)
